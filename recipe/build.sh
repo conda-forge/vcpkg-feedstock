eaee1b9381be485e42a6705c6462b7678f7484f7
Pavel Zwerschke <pavelzw@gmail.com>
2022-09-02 15:27:23 +0200

    Add Library to paths in build step

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,11 @@
-mkdir -p $PREFIX/share/vcpkg
+mkdir -p $PREFIX/Library/share/vcpkg
 
-mv $SRC_DIR/ports $PREFIX/share/vcpkg/
-mv $SRC_DIR/scripts $PREFIX/share/vcpkg/
-mv $SRC_DIR/triplets $PREFIX/share/vcpkg/
-mv $SRC_DIR/versions $PREFIX/share/vcpkg/
-cp $SRC_DIR/.vcpkg-root $PREFIX/share/vcpkg/.vcpkg-root
-cp $SRC_DIR/LICENSE.txt $PREFIX/share/vcpkg/LICENSE.txt
+mv $SRC_DIR/ports $PREFIX/Library/share/vcpkg/
+mv $SRC_DIR/scripts $PREFIX/Library/share/vcpkg/
+mv $SRC_DIR/triplets $PREFIX/Library/share/vcpkg/
+mv $SRC_DIR/versions $PREFIX/Library/share/vcpkg/
+cp $SRC_DIR/.vcpkg-root $PREFIX/Library/share/vcpkg/.vcpkg-root
+cp $SRC_DIR/LICENSE.txt $PREFIX/Library/share/vcpkg/LICENSE.txt
 
 # Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
 # This will allow them to be run on environment activation.
