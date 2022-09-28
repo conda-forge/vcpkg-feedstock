9728caeb95fe1fb1f5801e1c3b5829bb406b5016
Pavel Zwerschke <pavel.zwerschke@quantco.com>
2022-09-28 18:25:26 +0200

    Conditional library prefix

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -1,1 +1,7 @@
-export VCPKG_ROOT=${CONDA_PREFIX}/Library/share/vcpkg/
+OS=$(uname)
+if [[ "$OS" = "Linux" || "$OS" = "Darwin" ]]; then
+    LIBRARY_PREFIX="$PREFIX"
+else
+    LIBRARY_PREFIX="$PREFIX/Library"
+fi
+export VCPKG_ROOT=${LIBRARY_PREFIX}/share/vcpkg/
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,17 @@
-mkdir -p $PREFIX/Library/share/vcpkg
+OS=$(uname)
+if [[ "$OS" = "Linux" || "$OS" = "Darwin" ]]; then
+    LIBRARY_PREFIX="$PREFIX"
+else
+    LIBRARY_PREFIX="$PREFIX/Library"
+fi
+mkdir -p $LIBRARY_PREFIX/share/vcpkg
 
-mv $SRC_DIR/ports $PREFIX/Library/share/vcpkg/
-mv $SRC_DIR/scripts $PREFIX/Library/share/vcpkg/
-mv $SRC_DIR/triplets $PREFIX/Library/share/vcpkg/
-mv $SRC_DIR/versions $PREFIX/Library/share/vcpkg/
-cp $SRC_DIR/.vcpkg-root $PREFIX/Library/share/vcpkg/.vcpkg-root
-cp $SRC_DIR/LICENSE.txt $PREFIX/Library/share/vcpkg/LICENSE.txt
+mv $SRC_DIR/ports $LIBRARY_PREFIX/share/vcpkg/
+mv $SRC_DIR/scripts $LIBRARY_PREFIX/share/vcpkg/
+mv $SRC_DIR/triplets $LIBRARY_PREFIX/share/vcpkg/
+mv $SRC_DIR/versions $LIBRARY_PREFIX/share/vcpkg/
+cp $SRC_DIR/.vcpkg-root $LIBRARY_PREFIX/share/vcpkg/.vcpkg-root
+cp $SRC_DIR/LICENSE.txt $LIBRARY_PREFIX/share/vcpkg/LICENSE.txt
 
 # Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
 # This will allow them to be run on environment activation.
