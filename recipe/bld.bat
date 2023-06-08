ffcddf0b2b5e1a31b374164689b8b437eb6a94ae
AntoinePrv <AntoinePrv@users.noreply.github.com>
2023-06-08 10:09:23 +0200

    Use copy not move

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,16 +4,16 @@ setlocal enableextensions
 IF NOT EXIST %LIBRARY_PREFIX%\share\vcpkg MKDIR %LIBRARY_PREFIX%\share\vcpkg
 if errorlevel 1 exit 1
 
-MOVE %SRC_DIR%\ports %LIBRARY_PREFIX%\share\vcpkg\
+COPY %SRC_DIR%\ports %LIBRARY_PREFIX%\share\vcpkg\
 if errorlevel 1 exit 1
 
-MOVE %SRC_DIR%\scripts %LIBRARY_PREFIX%\share\vcpkg\
+COPY %SRC_DIR%\scripts %LIBRARY_PREFIX%\share\vcpkg\
 if errorlevel 1 exit 1
 
-MOVE %SRC_DIR%\triplets %LIBRARY_PREFIX%\share\vcpkg\
+COPY %SRC_DIR%\triplets %LIBRARY_PREFIX%\share\vcpkg\
 if errorlevel 1 exit 1
 
-MOVE %SRC_DIR%\versions %LIBRARY_PREFIX%\share\vcpkg\
+COPY %SRC_DIR%\versions %LIBRARY_PREFIX%\share\vcpkg\
 if errorlevel 1 exit 1
 
 COPY %SRC_DIR%\.vcpkg-root %LIBRARY_PREFIX%\share\vcpkg\.vcpkg-root
@@ -25,9 +25,9 @@ if errorlevel 1 exit 1
 :: Copy the [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
 :: This will allow them to be run on environment activation.
 for %%F in (activate deactivate) DO (
-    if not exist %PREFIX%\etc\conda\%%F.d mkdir %PREFIX%\etc\conda\%%F.d
-    copy %RECIPE_DIR%\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
+    if NOT EXIST %PREFIX%\etc\conda\%%F.d MKDIR %PREFIX%\etc\conda\%%F.d
+    COPY %RECIPE_DIR%\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
     :: Copy unix shell activation scripts, needed by Windows Bash users
-    copy %RECIPE_DIR%\%%F.sh %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.sh
+    COPY %RECIPE_DIR%\%%F.sh %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.sh
 )
 if errorlevel 1 exit 1
